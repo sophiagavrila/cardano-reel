547473853f124dee3a3f2105b55a41507bb8ad2f
Sophia <msophiagavrila@gmail.com>
2021-08-15 13:11:57 -0400

    elaborate on properties of each english article in ArticleList

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,10 +1,41 @@
 import React from "react";
-import { List } from "semantic-ui-react";
+import { List, Grid, Header } from "semantic-ui-react";
 
 const ArticleItem = (props) => {
 
     const { article } = props;
-    return <List.Item>{article.title}</List.Item>
+    return <List.Item>{article.title}
+
+        <Grid>
+            <Grid.Column
+                width={11}
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyCOntent: "flex-start",
+                }}
+            >
+
+                <Header as="h3">{article.title}</Header>
+                <List.Description style={{ margin: "20px 0" }}>
+                    {article.description}
+                </List.Description>
+
+                <List bulleted horizontal>
+
+                    <List.Item>
+                        <a href={article.url}>{article.source.name}</a>
+                    </List.Item>
+                    <List.Item>{article.publishedAt.split("T")[0]}</List.Item>
+
+                </List>
+
+            </Grid.Column>
+
+        </Grid>
+
+
+    </List.Item>
 
 }
 
