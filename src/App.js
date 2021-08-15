4a35fa1aa1824c1f04118fd2fc386c9540fa0a88
Sophia <msophiagavrila@gmail.com>
2021-08-15 12:11:46 -0400

    fix typo in fetch call

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import { getAdaArticles } from './api';
 import './App.css';
 
 function App() {
+  getAdaArticles();
+
   return (
+
     <div>
       We'll add our code here!
     </div>
--- src/api.js
+++ src/api.js
@@ -1,14 +1,24 @@
 import { NEWS_API_KEY; } from "./config"
 
-
+/**
+ * We create and export an arrow function export const getBitcoinArticles = async () => {}. 
+ * The keyword async declares that the function we create is an asynchronous function. 
+ * This keyword is important as we are working with asynchronous code, when we’re 
+ * connecting to the News API. async is required in order to use await.
+ */
 export const getAdaArticles = async () => {
 
     try {
         const response = await fetch(
-            // TODO: change "bitcoin" -> "cardano" after insuring that it works
-            `https://newsapi.org/v2/everything?q=bitcoin&apiKey={NEWS_API_KEY}`
-        )
+            `https://newsapi.org/v2/everything?q=cardano&apiKey=${NEWS_API_KEY}`
+        );
+
+        // The response that we receive from the API is just a HTTP response. 
+        // To extract the JSON body content from the response, we use the json() method.
+        const json = await response.json();
+        console.log(json);
+
+    } catch (error) {
+        console.log(error)
     }
-
-
 }
